172d6bc98e1cd7d636c1117e6e988ddad80db250
Kim Brugger <kbr@brugger.dk>
2015-04-29 10:06:00 +0100

    program that runs a set of commands in serial

---
+++ serial.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# 
+# script that runs a function with a set of infiles in serial
+# 
+# 
+# Kim Brugger (29 Apr 2015), contact: kim@brugger.dk
+
+import sys
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+import pipeliners
+
+if (len(sys.argv) == 1):
+    print "USAGE: serial.py [program to run] [file(s) to run on]"
+    exit()
+
+program = sys.argv[1]
+
+for arg in range(2, len(sys.argv)):
+    infile = str(sys.argv[ arg ])
+    cmd = str(program) + " " + infile
+    print cmd
+    pipeliners.system_call('serial_call', cmd)
+
+

3b5145ff362861c2de0679caf5ca197f1e2b1677
Kim Brugger <kbr@brugger.dk>
2015-04-29 10:06:25 +0100

    some verbose printing functions

--- pipeliners.py
+++ pipeliners.py
@@ -4,21 +4,37 @@ import subprocess
 
 verbose_level = 0
 
-VERBOSE_LEVELS = {'DEBUG' : 1,
-                  'INFO'  : 2,
-                  'WARN'  : 3,
-                  'ERROR' : 4,
-                  'FATAL' : 5}
+VERBOSE_LEVELS = {'DEBUG' : 0,
+                  'INFO'  : 1,
+                  'WARN'  : 2,
+                  'ERROR' : 3,
+                  'FATAL' : 4}
+REV_VERBOSE_LEVELS = { 0 : 'DEBUG',
+                       1 : 'INFO',
+                       2 : 'WARN',
+                       3 : 'ERROR',
+                       4 : 'FATAL' };
 
 
-def verbose_level( new_level ):
+
+def set_verbose_level( new_level ):
 
     new_level = new_level.upper()
 
     if ( new_level in VERBOSE_LEVELS ):
         verbose_level = new_level
     else:
-        print "Unknown verbosity level: " new_level
+        print "Unknown verbosity level: " + new_level
+
+
+    
+
+def verbose_print( message, level ):
+
+    if (VERBOSE_LEVELS[ level ] > verbose_level):
+        return
+
+    print REV_VERBOSE_LEVELS[ verbose_level ] + " :: " + message
 
 
     
@@ -28,13 +44,12 @@ def verbose_level( new_level ):
 
 def system_call( step_name, cmd ):
 
+    verbose_print(cmd, 'DEBUG')
+
     try:
         subprocess.check_call(cmd, shell=True)
 
     except subprocess.CalledProcessError as scall:
-        if ( VERBOSE == "DEBUG"):
-            print "Script failed at %s stage - exit code was %s, ouput = %s" % (step_name, scall.returncode, scall.output) 
 
-        if ( VERBOSE == "INFO"):
-            print "Script failed at %s stage - exit code was %s" % (step_name, scall.returncode)
-        exit()
+        verbose_print("Script failed at %s stage - exit code was %s, ouput = %s" % (step_name, scall.returncode, scall.output), 'DEBUG')
+        verbose_print("Script failed at %s stage - exit code was %s" % (step_name, scall.returncode), 'INFO')

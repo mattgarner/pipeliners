5f36fecfbf978419738b5836707e1a62b5ea2627
ngleadall <nick.gleadall@googlemail.com>
2015-05-08 14:43:30 +0100

    finsihed unclip pipeline - KIM BROKE DEDUPING

--- pipes/Unclipping_analysis_pipeline/unclip_analysis_kb_example.py
+++ pipes/Unclipping_analysis_pipeline/unclip_analysis_kb_example.py
@@ -3,73 +3,97 @@
 # Pipeline for producing clipped and unclipped bamfiles from sample_name's
 # 23st April 2015
 # Nicholas S. Gleadall - ng384@cam.ac.uk
+# Kim Brugger - kim@brugger.dk
 
 # print "Hello World!"
 
 #------------------------------------------------------------
 # Importing Modules
 
+import tempfile
 import sys
 import subprocess
 import os
 import re
-sys.path.append("/Users/Nick/github/pipeliners/modules")
+sys.path.append("/data/VMshare/github/pipeliners/modules")
 
 import pipeliners
 
-psc = pipeliners.system_call("")
+
 
 #------------------------------------------------------------
 # Setting globals / functions
 
-def cutadapt ( infile, outfile):
-    return "/software/packages/cutadapt-1.1/bin/cutadapt -b TGTAGAACCATGTCGTCAGTGT -b AGACCAAGTCTCTGCTACCGT "  + infile  +" | gzip -c  > analysis/" + outfile
+def directory_setup(): 
+	# setus up a directory with random ID #
+	tempfile.makestemp(prefix= "Unclip Analysis - ")
+	
+def cutadapt( infile, outfile):
+	# Removes sequencing adapters
+    return "/software/packages/cutadapt-1.1/bin/cutadapt -b TGTAGAACCATGTCGTCAGTGT -b AGACCAAGTCTCTGCTACCGT "  + infile  + " | gzip -c  > " + outfile
 
-#def smalt ( infile1, infile2, outfile):
+def smalt( infile1, infile2, outfile):
     # Align the reads to the reference (run smalt-0.7.6 to see all options)
-#    return "/software/bin/smalt_0.7.6 map -f samsoft /refs/HIV/K03455_s1k6 " + infile1 + " " + infile2 + "| samtools view -Sb -  > " + outfile
+    return "/software/bin/smalt_0.7.6 map -f samsoft /refs/HIV/K03455_s1k6 " + infile1 + " " + infile2 + " > " + outfile
 
-#def remove_adapters(infile, outfile1):
+def sam_to_bam(infile, outfile):
+	# Converts samfile to bamfile 
+	return "/software/bin//samtools view -Sb " + infile + " -o " + outfile
 
-	# remove the sequencing adaptors
-#	return "/software/packages/cutadapt-1.1/bin/cutadapt -b TGTAGAACCATGTCGTCAGTGT -b AGACCAAGTCTCTGCTACCGT " + infile + " > " + outfile
+def sort_bamfile(infile, outfile):
+	# Sorts bamfile
+	return "/software/bin/samtools sort " + infile + " " + outfile
 
-#def sam_to_bam(infile, outfile):
+def deduplicate_bamfile(infile, outfile, outcsv):
+	# Marks and soft clips duplicate reads
+	return "/software/bin/picard -T MarkDuplicates I= " + infile + " O= " + outfile + " AS=true M= " + outcsv
 
-#	return "/software/bin/samtools view -Sb " + infile + " " + outfile
+def index_bamfile(infile):
+	# Index's the bam file so it can be viewed in IGV later on
+	return "/software/bin/samtools index " + infile
+	
+def HIV_alignment_fix(infile, outfile):
+	# Fix's HIV specific alignment issues
+	return "/software/packages/HIV-pipeline/scripts/bam_fix_indels.pl " + infile + " " + outfile
 
-#def sort_bamfile(infile, outfile):
-
-	# sort bamfile
-#	return "/software/bin/samtools sort " + infile + ".bam " + outfile + "_sorted"
-
-#def deduplicate_bamfile(infile, outfile, outcsv):
-
-	# Mark/remove duplicate reads
-#	return "/software/bin/picard -t MarkDuplicates I= " + infile + " O= " + outfile + " AS=true M=" + outcsv
-
-#def index_bamfile(infile):
-	# Index the bam file so it can be viewed in IGV later on
-#	return "/software/bin/samtools index " + infile
-
-#def unclip_bamfile(infile):
-	# Run unclip bamfile
-#	return "/software/bin/scripts/bam_unclip_bases.pl " + infile + ".bam"
+def unclip_bamfile(infile):
+	# Run's kims unclipping script
+	return "/software/bin/scripts/bam_unclip_bases.pl " + infile
 
 
 #------------------------------------------------------------
 # Define fastq's and get sample names.
-
 # Gets location of fastq
 if (len(sys.argv) == 1):
     print "Need's sample name as input"
     exit()
 
-
 sample = sys.argv[1]
-sample_name = re.sub(r"(.*).1.fq.gz", r"\1", sample_name)
-sample_name_unclipped = sample_name + "_unclipped"
+sample_name = re.sub(r"(.*).1.fq.gz", r"\1", sample)
+sample_name_uc = sample_name + "_unclipped"
 
-pipeliners.system_call('cutadapt', 'sample')
+# Preparation of bamfile + unclipped bamfile  
+pipeliners.system_call('Cut Adapters', cutadapt(sample_name + ".1.fq.gz", sample_name + "_ar.1.fq.gz"))
+pipeliners.system_call('Cut Adapters', cutadapt(sample_name + ".2.fq.gz", sample_name + "_ar.2.fq.gz"))
+pipeliners.system_call('SMALT alignment', smalt(sample_name + "_ar.1.fq.gz", sample_name + "_ar.2.fq.gz", sample_name + ".sam"))
+pipeliners.system_call('Convert samfile to bamfile', sam_to_bam(sample_name + ".sam", sample_name + ".bam"))
+
+pipeliners.system_call('Unclipping bamfile', unclip_bamfile(sample_name + ".bam"))
+
+
+# Bamfile processing pipeline 
+pipeliners.system_call('Sort the bamfile', sort_bamfile(sample_name+ ".bam", sample_name + "_sorted"))
+pipeliners.system_call('Deduplicate bamfile', deduplicate_bamfile(sample_name + "_sorted.bam", sample_name + "_rmdups.bam", sample_name + "_rmdup.csv"))
+pipeliners.system_call('Index bamfile', index_bamfile(sample_name + "_rmdups.bam"))
+pipeliners.system_call('Fix HIV alignment', HIV_alignment_fix(sample_name + "_rmdups.bam", sample_name + "_fixed.bam"))
+pipeliners.system_call('Index bamfile', index_bamfile(sample_name + "_fixed.bam"))
+
+# Bamfile_unclipped processing pipeline 
+pipeliners.system_call('Sort the unclipped bamfile', sort_bamfile(sample_name_uc+ ".bam", sample_name_uc + "_sorted"))
+pipeliners.system_call('Deduplicate unclipped bamfile', deduplicate_bamfile(sample_name_uc + "_sorted.bam", sample_name_uc + "_rmdups.bam", sample_name_uc + "_rmdup.csv"))
+pipeliners.system_call('Index unclipped bamfile', index_bamfile(sample_name_uc + "_rmdups.bam"))
+pipeliners.system_call('Fix HIV alignment (unclipped)', HIV_alignment_fix(sample_name_uc + "_rmdups.bam", sample_name_ + "_fixed.bam"))
+pipeliners.system_call('Index unclipped fixed bamfile', index_bamfile(sample_name_uc + "_fixed.bam"))
+
 
 exit()
